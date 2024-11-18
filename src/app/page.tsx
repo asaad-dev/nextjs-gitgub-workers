5ec9ea80d8e717f0137b9c8a8d8faa5a3c259313
asaad <asaadmohamed8756@gmail.com>
2024-11-18 02:44:00 +0100

    Add more content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
   return (
    <div>
     <h1 className="text-3xl font-semibold">Nextjs GitHub Workers</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iure ipsam, maiores, sapiente culpa cumque quasi ad aliquam exercitationem alias a! Esse, quo nihil deserunt quas commodi veritatis voluptates veniam!
+   Quos sint, soluta consequatur enim quibusdam voluptate numquam eaque quaerat saepe. At dicta ea natus perspiciatis illo nihil, vel beatae doloremque laudantium sapiente aperiam nostrum sed voluptate aliquid, quae vitae.
+   Pariatur molestiae exercitationem odio necessitatibus officia voluptate soluta deserunt maxime blanditiis debitis velit eligendi dolores aspernatur, fuga itaque harum perspiciatis eaque non expedita? Earum repellat excepturi incidunt ea delectus modi.</p>
    </div>
+
   );
 }
